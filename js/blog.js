ea443c3db88db41aa553e46ff66239a56c2547b0
Kevin Yeh <kevinyeah@utexas.edu>
2014-12-13 15:21:49 -0500

    post cleanup and blog scrolling dynamics

--- js/blog.js
+++ js/blog.js
@@ -1,15 +1,52 @@
+function current_post_indicator() {
+    var window_top = $(window).scrollTop();
+    var post_index = 0;
+    var posts = $('.post');
+    var links = $('.link a');
+
+    if(window_top + $(window).height() == $(document).height()) {
+        post_index = links.length - 1;
+    } else {
+        for (i = 1; i < posts.length; i++) {
+            var post_top = $(posts[i]).offset().top - 2;
+            if (window_top > post_top) {
+                post_index++;
+            }
+        }
+    }
+ 
+    for (i = 0; i < links.length; i++) {
+        if (i == post_index) {
+            var post_top = $(posts[i]).offset().top - 2;
+            var read_location = window_top - post_top;
+
+            var opacity_cutoff = Math.min($(posts[i]).height(), 400);
+            var top_read_ratio =  read_location / opacity_cutoff;
+            var bot_read_ratio = ($(posts[i]).height() - read_location) / opacity_cutoff;
+            var read_ratio = Math.min(top_read_ratio, bot_read_ratio);
+
+            $(links[i]).addClass('menu-active');
+            $(links[i]).css({ 'opacity' : Math.max(0.5, read_ratio)});
+        } else {
+            $(links[i]).removeClass('menu-active');
+            $(links[i]).css({ 'opacity' : 0.5});
+        }
+    }
+}
+
 function sticky_relocate() {
     var window_top = $(window).scrollTop();
     var div_top = $('#posts_anchor').offset().top;
     if (window_top > div_top) {
         $('#posts').addClass('sticky');
-        $('#post_menu_header').addClass('animated fadeInDown');
-        $('#post_menu_header').removeClass('animated fadeOutUp');
     } else {
         $('#posts').removeClass('sticky');
-        $('#post_menu_header').addClass('animated fadeOutUp');
-        $('#post_menu_header').removeClass('animated fadeInDown');
     }
+}
+
+function on_scroll() {
+    sticky_relocate();
+    current_post_indicator();
 }
 
 $(function () {
@@ -20,6 +57,6 @@ $(function () {
         $(this).css("animation-delay", delay + "s");
         delay += 0.2;
     });
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
+    $(window).scroll(on_scroll);
+    on_scroll();
 });
