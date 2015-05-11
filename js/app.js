0f1eb55c0e0ef856bfaac6a9259e9514d6cd8e10
Kevin Yeh <kevinyeah@utexas.edu>
2015-05-11 02:19:15 -0500

    Fix cover

--- js/app.js
+++ js/app.js
@@ -50,10 +50,32 @@
 
     jQuery("time.timeago").timeago();
 
-    $.stellar({
-        horizontalScrolling: false,
-        verticalOffset:0
-    });
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+    if( !isMobile.any() ){
+        $.stellar({
+            horizontalScrolling: false,
+            verticalOffset:0
+        });
+    }
 
     jQuery(function() {
         $(".fancybox").fancybox({
